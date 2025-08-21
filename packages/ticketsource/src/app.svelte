c82c8094f3bcba09eae209de0c065cd02d3511d2
Darby Manning <me@darbymanning.com>
2025-08-21 07:16:31 +0100

    Add Ticketsource

---
+++ packages/ticketsource/src/app.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Input, Label } from 'shared'
+	import { HeadingManager as TicketSource } from './app.svelte.js'
+
+	const ticketsource = new TicketSource()
+</script>
+
+<div class="grid gap-2">
+	<select
+		class="rounded outline-none focus-visible:border-ring border-input border bg-input-background min-h-11.5 w-full disabled:opacity-50 disabled:cursor-wait"
+		bind:value={ticketsource.content.event}
+		onchange={ticketsource.setup_event}
+		disabled={ticketsource.loading}
+	>
+		<option value="">{ticketsource.loading ? 'Loading events…' : 'Select an event…'}</option>
+		{#if ticketsource.unique_events.length}
+			<optgroup label="Events">
+				{#each ticketsource.unique_events as event (event.id)}
+					{#if !event.attributes.archived && event.attributes.activated}
+						<option value={event.id}>
+							{event.attributes.name}
+							{#if event.count && event.count > 1}
+								({event.count} dates)
+							{/if}
+						</option>
+					{/if}
+				{/each}
+			</optgroup>
+		{/if}
+	</select>
+	{#if ticketsource.content.dates?.length}
+		<div class="grid gap-2" role="group">
+			{#each ticketsource.content.dates as date (date.id)}
+				<legend class="text-lg">
+					{date.start}
+				</legend>
+				<div class="grid gap-2">
+					<Label for="price_{date.id}">Price</Label>
+					<Input id="price_{date.id}" bind:value={date.price} oninput={ticketsource.update} />
+				</div>
+			{/each}
+		</div>
+	{/if}
+</div>
---
+++ packages/ticketsource/src/async.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import App from './app.svelte'
+</script>
+
+<svelte:boundary>
+	{#snippet pending()}
+		<p>Loading...</p>
+	{/snippet}
+	<App />
+</svelte:boundary>
