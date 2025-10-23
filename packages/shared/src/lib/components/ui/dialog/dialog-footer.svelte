1e822a7e996c504bd205e142dea02ef749eceaeb
Darby Manning <me@darbymanning.com>
2025-10-23 08:18:43 +0100

    Enhance `shared` package with Tailwind CSS integration and new components
    - Setup Tailwind on the project so we can ensure shadcn-svelte is working as expected isolated from any other packages
    - Add components:
      - Accordion
      - Dialog
      - Dropdown Menu
      - Scroll Area
      - Sonner

---
+++ packages/shared/src/lib/components/ui/dialog/dialog-footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { cn, type WithElementRef } from '$lib/utils.js'
+	import type { HTMLAttributes } from 'svelte/elements'
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props()
+</script>
+
+<div
+	bind:this={ref}
+	data-slot="dialog-footer"
+	class={cn(
+		'-m-6 mt-0 flex flex-col-reverse gap-2 border-t bg-accent p-4 px-6 sm:flex-row sm:justify-end',
+		className
+	)}
+	{...restProps}
+>
+	{@render children?.()}
+</div>
